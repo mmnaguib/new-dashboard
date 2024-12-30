ee21aeac494acaab87c274f90608c147c0485179
mmnaguib <mnaguib126@gmail.com>
2024-12-30 14:56:07 +0200

    .

--- src/index.tsx
+++ src/index.tsx
@@ -3,16 +3,13 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { CartProvider } from "./utils/CartContext";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <CartProvider>
-      <App />
-    </CartProvider>
+    <App />
   </React.StrictMode>
 );
 reportWebVitals();
